b8c8e00a79f9d00b9b86214d1d79ee9406728bd5
Jenny K <jkam17@gmail.com>
2018-08-23 01:51:02 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,18 +3,18 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
 
       }, 
     :capulet => {
       #your key/value pairs here!
       
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
 
       }}
